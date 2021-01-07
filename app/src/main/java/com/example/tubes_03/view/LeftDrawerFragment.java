f6ee69580ef88ee353bbd45179b4f30b6d2d22b9
alfredapri <alfredapri2000@gmail.com>
2021-01-07 12:45:37 +0700

    Added missing LeftFragment skeleton

--- app/src/main/java/com/example/tubes_03/view/LeftDrawerFragment.java
+++ app/src/main/java/com/example/tubes_03/view/LeftDrawerFragment.java
@@ -1,6 +1,46 @@
 package com.example.tubes_03.view;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
-public class LeftDrawerFragment extends Fragment {
+import com.example.tubes_03.R;
+
+public class LeftDrawerFragment extends Fragment implements View.OnClickListener {
+    private FragmentListener fragmentListener;
+
+    public static LeftDrawerFragment newInstance(String title) {
+        LeftDrawerFragment fragment = new LeftDrawerFragment();
+        Bundle args = new Bundle();
+        args.putString("app_name", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_left, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof FragmentListener) {
+            this.fragmentListener = (FragmentListener)context;
+        }
+        else {
+            throw new ClassCastException(context.toString() + " must implement FragmentListener!");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
